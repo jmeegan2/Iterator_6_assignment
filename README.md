7330ee4002ad0da96e7074c2a88e73a4ecd95449
jmeegan2 <jmeegan8@gmail.com>
2022-10-05 22:54:44 -0400

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+In class assignment where we go over implementing an iterator and using ArrayList to store and display data.  
--- StringArray.java
+++ StringArray.java
@@ -1,41 +1,79 @@
 import java.util.Iterator;
 
-public class StringArray<T> implements Iterable<T> {
-    //Instance variable
-    String[] values;
+// Class called StringArray created
+// Make the class StringArray implements iterable
+public class StringArray implements Iterable {
+    // Instance variable (4)
+    private String[] values;
 
-    // Create a class constructor for StringArray Class
-    public StringArray() {
-
+    //Constructor for StringArray
+    public StringArray(String[] values) {
+        this.values = values;
     }
 
-
-    @Override
-    public Iterator<T> iterator() {
-        return null;
+    //Function for implement Iterable
+    public Iterator iterator() {
+        return new ArrayIterator(values);
     }
-
-
-    public class ArrayIterator<T> implements Iterator<T> {
+    private static class ArrayIterator implements Iterator {
+        String[] values;
         int current;
 
-        @Override
+        public ArrayIterator(String[] values) {
+            this.values = values;
+            current = 0;
+        }
         public boolean hasNext() {
-            return false;
+            return current < values.length && values[current] != null;
         }
 
-        @Override
-        public T next() {
-            return null;
+
+        public String next() {
+            String string = values[current];
+            current = current + 1;
+            return string;
         }
     }
 
+    // Inner class iterator traverses only string elements equal or longer
+    // then 4 characters
+    // not tested
+    private static class LongStringIterator implements Iterator {
+        String[] values;
+        int current;
+
+        public LongStringIterator(String[] values) {
+            this.values = values;
+            current = 0;
+        }
+
+        public boolean hasNext() {
+            return current < values.length && values[current] != null;
+        }
+
+        public Object next() {
+            String string = values[current];
+            current = current + 1;
+
+            if (string.length() > 4) {
+                return string;
+            } else {
+                return null;
+            }
+        }
+    }
+
+
+    // Main
     public static void main(String[] args) {
+        String[] stepOneInput = new String[]{"One", "Two", "Three", "Four", "Five"};
+        StringArray output = new StringArray(stepOneInput);
 
-        StringArray Obj = new StringArray();
+        for (Object content : output) {
+            System.out.println(content);
+        }
 
-        Obj.add("one");
+
     }
-
 
 }
